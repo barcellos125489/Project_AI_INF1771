27fa6e57ae2e41fbc9b5f8731fa3a002c1592ae5
Marreco202 <76889373+Marreco202@users.noreply.github.com>
2024-10-02 23:02:29 -0300

    rascunho 2 grafo.py

--- grafo.py
+++ grafo.py
@@ -1,13 +1,20 @@
 from __future__ import annotations
 
+global meu_id
+meu_id = 0
 
 class Node:
+    meu_id : int
     lista_adj : list
     tipo : str
 
     def __init__(self, tipo: str) -> None:
+        global meu_id
+        self.meu_id = meu_id
         self.tipo = tipo
         lista_adj = []
+
+        meu_id+=1
 
     @classmethod
     def add_vizinhos(self, vizinhos : list) -> None:
@@ -32,3 +39,44 @@ def matrix_to_node(matrix: list) -> list:
 
 
     return
+
+def get_grafo(node_matrix : list) -> dict:
+    '''
+    Recebe matriz lista de listas. Cada elemento é um objeto Node
+
+    Retorna lista de adjacencias em forma de dicionário. Chaves são os próprios nós e os valores são listas de nós adjacentes as chaves
+    '''
+    
+    grafo = {}
+
+    for i in range(0,len(node_matrix)):
+        for j in range(0,len(node_matrix[i])):
+            vizinhos = []
+            
+            try:
+                cima = node_matrix[i][j-1] #cima
+                vizinhos.append(cima)
+            except:
+                pass
+
+            try:
+                baixo = node_matrix[i][j+1] #baixo
+                vizinhos.append(baixo)
+            except:
+                pass
+
+            try:
+                esquerda = node_matrix[i-1][j] #esquerda
+                vizinhos.append(esquerda)
+            except:
+                pass
+            try:
+                direita = node_matrix[i+1][j] #direita
+                vizinhos.append(direita)
+            except:
+                pass
+
+            node_matrix[i][j].add_vizinhos(vizinhos)
+            grafo[node_matrix[i][j]] = vizinhos
+
+    return grafo
